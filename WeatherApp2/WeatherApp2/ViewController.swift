db96e4fe76997a538f8e7ffae5bcf1b21f217647
seitovnurlan <seitov@gmail.com>
2021-03-05 20:53:17 +0600

    Project is created

---
+++ WeatherApp2/WeatherApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp2
+//
+//  Created by Nurlan Seitov on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
